f4b139895bcaebc06fc73e611659f969c4cd2e4c
Ryota ARAKAWA <r_arakawa@toa.co.jp>
2024-12-04 09:24:26 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -4,3 +4,12 @@ Scenario('example.com にアクセスする', ({ I }) => {
   I.amOnPage('https://example.com');
   I.see('Example Domain');
 });
+
+Scenario('Webサイトを開きログインする', ({ I }) => {
+  I.amOnPage('https://fastify-webapp-sample-production-1091.up.railway.app/')
+  I.click('ログインする')
+  I.fillField('ユーザー名', 'user1')
+  I.fillField('パスワード', 'super-strong-passphrase')
+  I.click('ログイン')
+  I.see('user1 さん')
+})
